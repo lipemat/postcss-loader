86c850ce9018746f64897a5b9a48e10b52842d8c
lipemat <mat@matlipe.com>
2020-12-19 16:59:19 -0700

    Update tests to match latest code
    Update the sourcemap test to match our fixed version.

--- test/options/sourceMap.test.js
+++ test/options/sourceMap.test.js
@@ -47,7 +47,15 @@ describe('Options', () => {
       const { source } = stats.toJson().modules[1]
 
       expect(source).toEqual(
-        'module.exports = "a { color: rgba(255, 0, 0, 1.0) }\\n\\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInRlc3QvZml4dHVyZXMvY3NzL3N0eWxlLmNzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxJQUFJLDJCQUFZLEVBQUUiLCJmaWxlIjoidGVzdC9maXh0dXJlcy9jc3Mvc3R5bGUuY3NzIiwic291cmNlc0NvbnRlbnQiOlsiYSB7IGNvbG9yOiBibGFjayB9XG4iXX0= */"'
+        'module.exports = "a { color: rgba(' +
+        '255, 0, 0, 1.0) }\\n\\n/*# sourceMappingUR' +
+        'L=data:application/json;base64,eyJ2ZXJzaW9' +
+        'uIjozLCJzb3VyY2VzIjpbInRlc3QvZml4dHVyZXMvY' +
+        '3NzL3N0eWxlLmNzcyJdLCJuYW1lcyI6W10sIm1hcHB' +
+        'pbmdzIjoiQUFBQSxJQUFJLDRCQUFhIiwiZmlsZSI6I' +
+        'nRlc3QvZml4dHVyZXMvY3NzL3N0eWxlLmNzcyIsInN' +
+        'vdXJjZXNDb250ZW50IjpbImEgeyBjb2xvcjogYmxhY' +
+        '2sgfVxuIl19 */"'
       )
 
       expect(source).toMatchSnapshot()
